67d0edac5f0e49c80de01290e652bd4204c7a86c
Volha Melayok <neiwra@gmail.com>
2024-09-22 15:22:12 +0200

    feat: add unusual-console.js

---
+++ unusual-console.js
@@ -0,0 +1,5 @@
+console.group( '\x1b[35m%s\x1b[0m','Animals:');
+console.log(`${+'a'}aki`)
+console.log(`${([] + {})[5]}${(![]+[])[+!+[]]}${([] + {})[6]}`);
+console.log(`${[...'d']}${['o']}${[...'g']}`)
+console.groupEnd();
